8d1ad81c3a973a1698d14732f31e314b725ff762
Valeriy Vartumyan <RulianValeriy@yandex.ru>
2022-07-01 00:01:05 +0300

    hot fix

--- CinemaDesktop/MVVM/ViewModel/RegistrationViewModel.cs
+++ CinemaDesktop/MVVM/ViewModel/RegistrationViewModel.cs
@@ -4,8 +4,6 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
-using CryptoDesktop.MVVM.Commands;
-using CryptoDesktop.UserControls;
 using DryIoc;
 
 namespace CinemaDesktop.MVVM.ViewModel;
