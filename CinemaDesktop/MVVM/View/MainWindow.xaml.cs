069de024f3511f837f06bf46317fafb640e40afc
Valeriy Vartumyan <RulianValeriy@yandex.ru>
2022-06-30 09:46:31 +0300

    Added MVVM

---
+++ CinemaDesktop/MVVM/View/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace CinemaDesktop
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+		{
+			if (e.LeftButton == MouseButtonState.Pressed)
+				DragMove();
+		}
+
+		private void Close_Click(object sender, RoutedEventArgs e)
+		{
+			Application.Current.Shutdown();
+		}
+
+		private void Maximaze_Click(object sender, RoutedEventArgs e)
+		{
+			Application.Current.MainWindow.WindowState = 
+				Application.Current.MainWindow.WindowState != WindowState.Maximized ?
+					WindowState.Maximized : WindowState.Normal;
+		}
+
+		private void Minimaze_Click(object sender, RoutedEventArgs e)
+		{
+			Application.Current.MainWindow.WindowState = WindowState.Minimized;
+		}
+	}
+}
