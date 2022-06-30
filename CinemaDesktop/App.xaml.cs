5169e61544788f9ce39a87f18cdcb360f9216a8d
Valeriy Vartumyan <RulianValeriy@yandex.ru>
2022-06-30 09:54:01 +0300

    App init

---
+++ CinemaDesktop/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using DryIoc;
+
+namespace CinemaDesktop
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static Container Container { get; set; }
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+			
+            Container = new Container();
+			
+            // Grpc client
+            //Views
+            Container.Register<Registration>(Reuse.Transient);
+            Container.Register<MainWindow>(Reuse.Transient);
+            //ViewModels
+			
+            var reg = Container.Resolve<MainWindow>();
+            reg.Show();
+        }
+    }
+}
