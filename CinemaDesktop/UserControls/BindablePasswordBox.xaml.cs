2b073ec0db15083117c5d2e5228164771e11dcc3
Valeriy Vartumyan <RulianValeriy@yandex.ru>
2022-06-30 09:54:11 +0300

    User controls added

---
+++ CinemaDesktop/UserControls/BindablePasswordBox.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Controls;
+using CryptoDesktop.Core.Interfaces;
+
+namespace CryptoDesktop;
+
+public partial class BindablePasswordBox : UserControl, IPasswordSupplier
+{
+    public BindablePasswordBox()
+    {
+        InitializeComponent();
+    }
+    public string GetPassword()
+    {
+        return pwdBox.Password;
+    }
+}
