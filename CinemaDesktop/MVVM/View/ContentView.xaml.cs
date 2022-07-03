d904273f80adb76edb2af9a6b21bde78f3582cad
Polina <vecherdobrii007@gmail.com>
2022-07-03 21:42:26 +0300

    Content view/viewmodel added

---
+++ CinemaDesktop/MVVM/View/ContentView.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace CinemaDesktop.MVVM.View;
+
+public partial class ContentView : UserControl
+{
+    public ContentView()
+    {
+        InitializeComponent();
+    }
+}
