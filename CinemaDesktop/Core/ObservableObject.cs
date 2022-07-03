38dfb27fcc49378e54fe81acb243d3f90254bc75
Polina <vecherdobrii007@gmail.com>
2022-07-03 21:40:54 +0300

    Observable Object added

---
+++ CinemaDesktop/Core/ObservableObject.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace CinemaDesktop.Core;
+
+public class ObservableObject: INotifyPropertyChanged
+
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
