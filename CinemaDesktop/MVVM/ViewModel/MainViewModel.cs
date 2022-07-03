0f41a941a2772b257aadb596fbb1e721df61660d
Polina <vecherdobrii007@gmail.com>
2022-07-03 21:44:39 +0300

    Main Window view/viewmodel added

---
+++ CinemaDesktop/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,45 @@
+﻿using CinemaDesktop.Core;
+using CinemaDesktop.MVVM.Commands;
+
+namespace CinemaDesktop.MVVM.ViewModel;
+
+public class MainViewModel: ObservableObject
+{
+    public RelayCommand ContentViewCommand { get; set; }
+    
+    public RelayCommand UserViewCommand { get; set; }
+
+    public ContentViewModel ContentVM;
+    
+    public UserViewModel UserVM;
+
+    private object _currentView;
+
+    public object CurrentView
+    {
+        get => _currentView;
+        set
+        {
+            _currentView = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public MainViewModel()
+    {
+        ContentVM = new ContentViewModel();
+        UserVM = new UserViewModel();
+        
+        CurrentView = ContentVM;
+
+        ContentViewCommand = new RelayCommand(o =>
+        {
+            CurrentView = ContentVM;
+        });
+        
+        UserViewCommand = new RelayCommand(o =>
+        {
+            CurrentView = UserVM;
+        });
+    }
+}
