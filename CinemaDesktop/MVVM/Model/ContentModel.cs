43d110b1bb1e5712c8578fa5f93d5d6219880a92
Polina Bureeva <83447398+vecherochek@users.noreply.github.com>
2022-07-04 18:04:07 +0300

    Creating content card (#13)
    * added new icons
    
    * added new button themes
    
    * updated
    
    * updated content card control
    
    * content card mvvm added
    
    * views updated
    
    * icons include csproj

--- CinemaDesktop/MVVM/Model/ContentModel.cs
+++ CinemaDesktop/MVVM/Model/ContentModel.cs
@@ -6,5 +6,22 @@ public class ContentModel
 {
     public string ContentName { get; set; }
     public string ImageSource { get; set; }
-    public string ContentGenre { get; set; }
+    
+    public string Cast { get; set; }
+    
+    public string Description { get; set; }
+    
+    public string IMDb { get; set; }
+    
+    public string RottenTomatoes { get; set; }
+    
+    public string  Data { get; set; }
+    
+    public string  Runtime { get; set; }
+
+    public string Directors { get; set; }
+    
+    public string Writers { get; set; }
+
+    
 }
---
+++ CinemaDesktop/MVVM/ViewModel/ContentCardViewModel.cs
@@ -0,0 +1,31 @@
+﻿using CinemaDesktop.MVVM.Model;
+
+namespace CinemaDesktop.MVVM.ViewModel;
+
+public class ContentCardViewModel
+{
+    public ContentModel Model { get; set; }
+    
+    public ContentCardViewModel(ContentModel model)
+    {
+        Model = model;
+    }
+
+    public ContentCardViewModel()
+    {
+        Model = new ContentModel
+        {
+            ContentName = "Гарри Поттер и филосовский камень",
+            ImageSource =
+                "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQXKskA949YDC85-yvsGsHYgkXOh_og1Lt12vyGa5KthpCXnvti",
+            Cast = "Richard Harris, Maggie Smith, Robbie Coltrane, Daniel Radcliffe, Rupert Grint, Emma Watson",
+            IMDb = "8.8",
+            RottenTomatoes = "87",
+            Data = "2001",
+            Runtime = "2h 32m",
+            Directors = "Chris Columbus",
+            Writers = "J.K. Rowling, Steve Kloves",
+            Description = "This is the tale of Harry Potter (Daniel Radcliffe), an ordinary eleven-year-old boy serving as a sort of slave for his aunt and uncle who learns that he is actually a wizard and has been invited to attend the Hogwarts School for Witchcraft and Wizardry. Harry is snatched away from his mundane existence by Rubeus Hagrid (Robbie Coltrane), the groundskeeper for Hogwarts, and quickly thrown into a world completely foreign to both him and the viewer. Famous for an incident that happened at his birth, Harry makes friends easily at his new school. He soon finds, however, that the wizarding world is far more dangerous for him than he would have imagined, and he quickly learns that not all wizards are ones to be trusted.—Carly"
+        };
+    }
+}
