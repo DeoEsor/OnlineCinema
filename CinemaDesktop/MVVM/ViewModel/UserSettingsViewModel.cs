21b71248ba45ff34761a28d3dff13adc00dc53ec
Polina <vecherdobrii007@gmail.com>
2022-07-05 21:56:49 +0300

    user settings mvvm init

---
+++ CinemaDesktop/MVVM/ViewModel/UserSettingsViewModel.cs
@@ -0,0 +1,28 @@
+﻿using CinemaDesktop.MVVM.Model;
+
+namespace CinemaDesktop.MVVM.ViewModel;
+
+public class UserSettingsViewModel
+{
+    public UserModel Model { get; set; }
+
+    public UserSettingsViewModel(UserModel model)
+    {
+        Model = model;
+    }
+    public UserSettingsViewModel()
+    {
+        Model = new UserModel
+        {
+            Name = "Скарлетт",
+            Surname = "Сорокина",
+            Nickname = "Да я кошка",
+            DateOfBirth = "123",
+            Email = "kakoitoemail@gmail.com",
+            Country = "Russia",
+            Password = "4yf56y56u6j",
+            ImageSource =
+                "https://sun9-24.userapi.com/impg/69p9XpWkujGND-ODbQAGq94aPcq2EYXn_uiRig/SjWdzeDD5Cg.jpg?size=1344x1792&quality=95&sign=ad9ccf8eb6f4d6eadde27f566aff10f6&type=album"
+        };
+    }
+}
