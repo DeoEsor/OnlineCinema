51a4bc82ce3f22381cdeebb97708f1442e96749f
Valeriy Vartumyan <60220969+DeoEsor@users.noreply.github.com>
2022-07-09 21:59:46 +0300

    Person rename to user (#24)

---
+++ OnlineCinema.Domain/Person/Interfaces/IUsers.cs
@@ -0,0 +1,8 @@
+﻿namespace OnlineCinema.Domain.Person.Interfaces;
+
+public interface IUsers
+{
+    Task Add(User user);
+    Task<IReadOnlyList<User>> GetListAsync();
+    Task<User> ChangeAsync(User user);
+}
