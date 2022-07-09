51c69d085c54f75a860de10a0a423b8985222bda
Polina Bureeva <83447398+vecherochek@users.noreply.github.com>
2022-07-09 19:43:55 +0300

    Creating user settings view (#20)
    * added new icons
    
    * added new element themes for user settings view
    
    * updated play button theme
    
    * updated user view/model
    
    * creating user settings view/viewmodel

--- CinemaDesktop/MVVM/Model/UserModel.cs
+++ CinemaDesktop/MVVM/Model/UserModel.cs
@@ -1,20 +1,96 @@
 ﻿using System;
-using UserService.Messages;
+using CinemaDesktop.Core;
 
 namespace CinemaDesktop.MVVM.Model;
 
-public class UserModel
+public class UserModel : ObservableObject
 {
-    public string Name { get; set; }
-    public string Surname { get; set; }
-    public string Country { get; set; }
-    public string Nickname { get; set; }
-    
-    public DateTime DateOfBirth { get; set; }
-    
-    public string ImageSource { get; set; }
-    
-    public string Email { get; set; }
-    
-    public string Password { get; set; }
+    private string _name;
+    private string _surname;
+    private string _country;
+    private string _nickName;
+    private DateTime _dateOfBirth;
+    private string _email;
+    private string _password;
+    private string _imageSource;
+
+    public string Name
+    {
+        get => _name;
+        set
+        {
+            _name = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public string Surname
+    {
+        get => _surname;
+        set
+        {
+            _surname = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public string Country
+    {
+        get => _country;
+        set
+        {
+            _country = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public string Nickname
+    {
+        get => _nickName;
+        set
+        {
+            _nickName = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public DateTime DateOfBirth
+    {
+        get => _dateOfBirth;
+        set
+        {
+            _dateOfBirth = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public string ImageSource
+    {
+        get => _imageSource;
+        set
+        {
+            _imageSource = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public string Email
+    {
+        get => _email;
+        set
+        {
+            _email = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public string Password
+    {
+        get => _password;
+        set
+        {
+            _password = value;
+            OnPropertyChanged();
+        }
+    }
 }
