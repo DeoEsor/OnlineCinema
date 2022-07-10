89c522a0606a5a8a38177c8d6f15532104e91616
Valeriy Vartumyan <RulianValeriy@yandex.ru>
2022-07-10 16:23:21 +0300

    added film db context

---
+++ OnlineCinema.DAL/Films/FilmsContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineCinema.Domain;
+
+namespace OnlineCinema.DAL;
+
+public class FilmsContext : DbContext
+{
+    public FilmsContext(DbContextOptions<FilmsContext> options) 
+        : base(options)
+    { }
+
+    public DbSet<Film> Films { get; set; }
+    public DbSet<Actor> Actors { get; set; }
+    public DbSet<Director> Directors { get; set; }
+    public DbSet<Writer> Writers { get; set; }
+    public DbSet<Serial> Serials { get; set; }
+    public DbSet<Season> Seasons { get; set; }
+    public DbSet<Episode> Episodes { get; set; }
+}
