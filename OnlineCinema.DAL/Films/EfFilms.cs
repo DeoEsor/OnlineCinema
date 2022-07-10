c3c9faca2a23fe264ea91ab46aa7e89d1562a37e
Valeriy Vartumyan <RulianValeriy@yandex.ru>
2022-07-10 16:23:55 +0300

    added films models repositories realizations

---
+++ OnlineCinema.DAL/Films/EfFilms.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineCinema.DAL.Core;
+using OnlineCinema.Domain;
+using OnlineCinema.Domain.Extensions;
+using OnlineCinema.Domain.Interfaces;
+
+namespace OnlineCinema.DAL;
+
+public class EfFilms : IFilms
+{
+    private readonly FilmsContext _context;
+    private readonly UnitOfWork<FilmsContext> _unit;
+
+    public EfFilms(UnitOfWork<FilmsContext> filmsUnitOfWork)
+    {
+        if (filmsUnitOfWork == null) throw new ArgumentNullException(nameof(filmsUnitOfWork));
+
+        _context = filmsUnitOfWork.Context;
+        _unit = filmsUnitOfWork;
+    }
+
+    public async Task AddAsync(Film item)
+    {
+        if (item == null) throw new ArgumentNullException(nameof(item));
+
+        await _context.Films.AddAsync(item);
+        await _unit.Commit();
+    }
+
+    public async Task DeleteAsync(Film item)
+    {
+        if (item == null) throw new ArgumentNullException(nameof(item));
+
+        _context.Films.Remove(item);
+        await _unit.Commit();
+    }
+
+    public async Task<IReadOnlyList<Film>> GetListAsync()
+    {
+        return await _context.Films
+            .OrderBy(p => p.FilmName)
+            .ToListAsync();
+    }
+
+    public async Task<Film> ChangeAsync(Film item)
+    {
+        if (item == null) throw new ArgumentNullException(nameof(item));
+
+        var film = _context.Films.FirstOrDefault(film => film.Id == item.Id)
+                      ?? throw new ArgumentException("No such Film in db", nameof(item));
+
+        film.Cast = item.Cast;
+        film.Description = item.Description;
+        film.FilmName = item.FilmName;
+        film.Writers = item.Writers;
+        film.Directors = item.Directors;
+        film.MagnetLink = item.MagnetLink;
+        
+        await _unit.Commit();
+
+        return film;
+    }
+
+    public async Task<IReadOnlyList<Film>> FindByNameAsync(string name, int limit = Int32.MaxValue)
+    {
+        if (name == null) throw new ArgumentNullException(nameof(name));
+
+        return await _context.Films
+            .Where(p => p.FilmName.LevenshteinDistance(name) < 10)
+            .OrderBy(p => p.FilmName.LevenshteinDistance(name))
+            .Take(limit)
+            .ToListAsync();
+    }
+
+    public async Task<IReadOnlyList<Film>> FindByGenreAsync(Genre genres, int limit = Int32.MaxValue)
+    {
+
+        return await _context.Films
+            .Where(p => p.Genres == genres)
+            .OrderBy(p => p.FilmName)
+            .Take(limit)
+            .ToListAsync();
+    }
+}
---
+++ OnlineCinema.DAL/Films/EfSerials.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineCinema.DAL.Core;
+using OnlineCinema.Domain;
+using OnlineCinema.Domain.Extensions;
+using OnlineCinema.Domain.Interfaces;
+
+namespace OnlineCinema.DAL;
+
+public class EfSerials : ISerials
+{
+    private readonly FilmsContext _context;
+    private readonly UnitOfWork<FilmsContext> _unit;
+
+    public EfSerials(UnitOfWork<FilmsContext> filmsUnitOfWork)
+    {
+        if (filmsUnitOfWork == null) throw new ArgumentNullException(nameof(filmsUnitOfWork));
+
+        _context = filmsUnitOfWork.Context;
+        _unit = filmsUnitOfWork;
+    }
+    
+    public async Task AddAsync(Serial item)
+    {
+        if (item == null) throw new ArgumentNullException(nameof(item));
+
+        await _context.Serials.AddAsync(item);
+        await _unit.Commit();
+    }
+
+    public async Task DeleteAsync(Serial item)
+    {
+        if (item == null) throw new ArgumentNullException(nameof(item));
+
+        _context.Serials.Remove(item);
+        await _unit.Commit();
+    }
+
+    public async Task<IReadOnlyList<Serial>> GetListAsync()
+    {
+        return await _context.Serials
+            .OrderBy(p => p.SerialName)
+            .ToListAsync();
+    }
+
+    public async Task<Serial> ChangeAsync(Serial serial)
+    {
+        if (serial == null) throw new ArgumentNullException(nameof(serial));
+
+        var item = _context.Serials.FirstOrDefault(item => item.Id == serial.Id)
+                      ?? throw new ArgumentException("No such Serial in db", nameof(serial));
+
+        item.Cast = serial.Cast;
+        item.Directors = serial.Directors;
+        item.Writers = serial.Writers;
+        item.SerialName = serial.SerialName;
+        item.Description = serial.Description;
+        item.Genres = serial.Genres;
+        item.PosterSource = serial.PosterSource;
+        await _unit.Commit();
+
+        return serial;
+    }
+
+    public async Task<IReadOnlyList<Serial>> FindByNameAsync(string name, int limit = Int32.MaxValue)
+    {
+        if (name == null) throw new ArgumentNullException(nameof(name));
+
+        return await _context.Serials
+            .Where(p => p.SerialName.LevenshteinDistance(name) < 10)
+            .OrderBy(p => p.SerialName.LevenshteinDistance(name))
+            .Take(limit) // Bug mb incorrect
+            .ToListAsync();
+    }
+
+    public async Task<IReadOnlyList<Serial>> FindByGenreAsync(Genre genres, int limit = Int32.MaxValue)
+    {
+        return await _context.Serials
+            .Where(p => p.Genres == genres)
+            .OrderBy(p => p.SerialName)
+            .Take(limit)
+            .ToListAsync();
+    }
+}
