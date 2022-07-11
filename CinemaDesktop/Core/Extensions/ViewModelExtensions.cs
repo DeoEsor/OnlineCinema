d55419457dee13791ffad0daa547edceb864cb71
Valeriy Vartumyan <RulianValeriy@yandex.ru>
2022-07-11 18:40:53 +0300

    Merge after fix

---
+++ CinemaDesktop/Core/Extensions/ViewModelExtensions.cs
@@ -0,0 +1,17 @@
+﻿using CinemaDesktop.MVVM.ViewModel;
+using DryIoc;
+
+namespace CinemaDesktop.Core.Extensions;
+
+public static class ViewModelExtensions
+{
+    public static void  ShowNewView(object newViewModel)
+    {
+        var mainViewModel = App.Container.Resolve<MainViewModel>();
+        mainViewModel.CurrentView = newViewModel;
+        if (!Equals(newViewModel, mainViewModel.VM.Peek()))
+        {
+            mainViewModel.VM.Push(mainViewModel.CurrentView);
+        }
+    }
+}
